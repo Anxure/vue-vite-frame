841a6e96642973a29f33e03ac008f1dca4c6b6ff
Anxure <505959291@qq.com>
2022-06-02 14:10:13 +0800

    others: 移除多余代码

--- mock/_createProdMockServer.ts
+++ mock/_createProdMockServer.ts
@@ -4,12 +4,11 @@
  * @Desc:
  * @Date: 2022-05-31 10:23:31
  * @LastEditors: Anxure
- * @LastEditTime: 2022-06-01 17:43:33
+ * @LastEditTime: 2022-06-02 09:17:46
  */
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 // 批量加载
 const modules = import.meta.globEager('./mock/module/*.ts')
-console.log(modules)
 const mockModules: Array<string> = []
 Object.keys(modules).forEach((key) => {
   mockModules.push(...modules[key].default)
