82fa7b6dd7c0a52bfda5d6144b720bc083af91ba
Anxure <505959291@qq.com>
2022-11-03 15:37:46 +0800

    feat: 新增mock线上开关

--- config/vite/plugins/mock.ts
+++ config/vite/plugins/mock.ts
@@ -4,7 +4,7 @@
  * @Desc:
  * @Date: 2022-06-01 10:40:55
  * @LastEditors: Anxure
- * @LastEditTime: 2022-11-03 13:51:01
+ * @LastEditTime: 2022-11-03 15:26:33
  */
 /**
  * @name ConfigMockPlugin
@@ -16,7 +16,7 @@
          ignore: /^\_/,
          mockPath: 'mock',
          localEnabled: !isBuild,
-         prodEnabled: false, //实际开发请关闭，会影响打包体积
+         prodEnabled: isBuild, //实际开发请关闭，会影响打包体积
          // https://github.com/anncwb/vite-plugin-mock/issues/9
          injectCode: `
         import { setupProdMockServer } from '../mock/_createProdMockServer';
