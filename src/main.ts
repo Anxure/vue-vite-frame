173ddca968265c71a304b8d963b8bbc2cea45a0a
Anxure <505959291@qq.com>
2022-11-03 15:24:08 +0800

    fix: 修复githubpage的模拟数据

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ import piniaStore from './store'
 import '@/assets/style/iconfont.css'
 import '@/assets/iconfont/iconfont'
 import '@/assets/style/common.less'
+import { setupProdMockServer } from '../mock/_createProdMockServer'
 // 如果你使用的 Vite，你可以使用 unplugin-vue-components 来进行按需加载。但是此插件无法处理非组件模块，如 message，这种组件需要手动加载：
 import 'ant-design-vue/es/message/style/css';
-import {message} from 'ant-design-vue'
+import { message } from 'ant-design-vue'
+if (import.meta.env.PROD) {
+  setupProdMockServer()
+}
 createApp(App)
   .use(piniaStore)
   .use(router)
